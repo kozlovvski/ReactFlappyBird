2479ddb95355b067a23233a6d4b9e5d485442fc6
kozlovvski <mcbiggos@gmail.com>
2019-07-07 00:41:31 +0200

    add help screen

--- src/components/HelpScreen.jsx
+++ src/components/HelpScreen.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 export default function HelpScreen() {
 	return (
 		<div id="help-screen">
-			
+			<img className="get-ready" src={require("images/get_ready.png")} alt="" />
+			<img className="help" src={require("images/help.png")} alt="" />
 		</div>
 	)
 }
